451f5539e41671bd3624c5857a5cf0c89865f493
Harshbn <120438037+Harshbn@users.noreply.github.com>
2024-01-25 22:42:03 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ProgressbarType } from 'ngx-bootstrap/progressbar';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
+import { zip } from 'rxjs';
+
+interface IStack {
+  type: ProgressbarType;
+  label: string;
+  value: number;
+  max: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers : [FormBuilder,
+    { provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+})
+export class AppComponent {
+  title = 'Asgn15';
+
+  constructor(public fbobj : FormBuilder)
+  {
+    this.randomStacked();
+  }
+
+  MarvellousForm = this.fbobj.group(
+    {
+      firstName:['A-Z/a-z',[Validators.required,Validators.minLength(2),Validators.pattern('[a-zA-Z ]*')]],
+    lastName:['',[Validators.required,Validators.minLength(2)]],
+    email:['',[Validators.required]],
+    phone:['',[Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern('[0-9]*')]],
+    Address:['',[Validators.required, Validators.minLength(20)]],
+    state:['',[Validators.required]],
+    City:['',[Validators.required,Validators.minLength(4)]],
+    zip:['',[Validators.required,Validators.minLength(6),Validators.maxLength(6),Validators.pattern('[0-9]*')]],
+    comments:['',[Validators.required, Validators.minLength(20)]]
+    }
+  )
+
+
+  type?: string;
+  stacked: IStack[] = [];
+
+  randomStacked(): void {
+    const types = ['success', 'info', 'warning', 'danger'];
+ 
+    this.stacked = [];
+    const n = Math.floor(Math.random() * 4 + 1);
+    for (let i = 0; i < n; i++) {
+      const index = Math.floor(Math.random() * 4);
+      const value = Math.floor(Math.random() * 27 + 3);
+      this.stacked.push(<IStack>{
+        value,
+        type: types[index],
+        label: value + ' %'
+      });
+    }
+  }
+
+
+  selected?: string;
+  states: string[] = [
+    'Maharashtra',
+    'Uttar Pradesh',
+    'Gujarat',
+  ];
+
+
+}
